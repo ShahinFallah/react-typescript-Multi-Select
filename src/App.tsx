640ed0aaeb4db0836f168a18582ed99661d0c834
ShahinFallah <shahinfallah2006@gmail.com>
2024-06-02 22:31:05 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl text-blue-400'>Hey</h1>
+    </>
+  )
+}
+
+export default App
