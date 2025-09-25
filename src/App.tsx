59cec0dd0284746436ede73b8658f2f409f7d0c2
ShahinFallah <shahinfallah2006@gmail.com>
2025-09-25 18:22:20 +0330

    chore: format code with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
-import { useState } from 'react'
-import Select, { SelectOption } from './Select'
+import { useState } from "react";
+import Select, { SelectOption } from "./Select";
 
 function App() {
   const options: SelectOption[] = [
-    { label: 'Apple', value: 1 },
-    { label: 'banana', value: 2 },
-    { label: 'coconut', value: 3 },
-    { label: 'orange', value: 4 },
-    { label: 'tangerine', value: 5 }
-  ]
+    { label: "Apple", value: 1 },
+    { label: "banana", value: 2 },
+    { label: "coconut", value: 3 },
+    { label: "orange", value: 4 },
+    { label: "tangerine", value: 5 },
+  ];
 
-  const [value1, setValue1] = useState<SelectOption | undefined>(options[0])
-  const [value2, setValue2] = useState<SelectOption[]>([options[0]])
+  const [value1, setValue1] = useState<SelectOption | undefined>(options[0]);
+  const [value2, setValue2] = useState<SelectOption[]>([options[0]]);
 
   return (
-    <div className='p-3 flex space-x-5'>
+    <div className="p-3 flex space-x-5">
       <div>
-        <Select value={value1} options={options} onChange={(o) => setValue1(o)} />
+        <Select
+          value={value1}
+          options={options}
+          onChange={(o) => setValue1(o)}
+        />
       </div>
       <div>
-        <Select multiple value={value2} options={options} onChange={(o) => setValue2(o)} />
+        <Select
+          multiple
+          value={value2}
+          options={options}
+          onChange={(o) => setValue2(o)}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
